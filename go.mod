2e56da9d17426e054850bef312b45b73eac1df3e
monotiller <18310129+monotiller@users.noreply.github.com>
2022-03-31 12:13:20 +0100

    Started OOP and added a menu

--- go.mod
+++ go.mod
@@ -3,3 +3,15 @@ module monotiller/go-upskilling
 go 1.18
 
 require github.com/mattn/go-sqlite3 v1.14.12
+
+require (
+	github.com/dixonwille/wmenu v4.0.2+incompatible
+	github.com/dixonwille/wmenu/v5 v5.1.0
+)
+
+require (
+	github.com/daviddengcn/go-colortext v0.0.0-20180409174941-186a3d44e920 // indirect
+	github.com/mattn/go-isatty v0.0.11 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	gopkg.in/dixonwille/wlog.v2 v2.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,28 +4,35 @@ import (
 	"bufio"
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
 
+	"github.com/dixonwille/wmenu"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
+func add(args ...string) {
 	database, _ :=
 		sql.Open("sqlite3", "./names.db")
 	statement, _ :=
 		database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
 	statement.Exec()
-
 	fmt.Print("Enter a name in the format: Firstname, Lastname: \n")
 	reader := bufio.NewReader(os.Stdin)
 	rawInput, _ := reader.ReadString('\n')
 	slicedInput := strings.Split(rawInput, ", ")
-
 	statement, _ =
 		database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
 	statement.Exec(slicedInput[0], slicedInput[1])
+
+	return
+}
+
+func view(args ...string) {
+	database, _ :=
+		sql.Open("sqlite3", "./names.db")
 	rows, _ :=
 		database.Query("SELECT id, firstname, lastname FROM people")
 	var id int
@@ -34,5 +41,41 @@ func main() {
 	for rows.Next() {
 		rows.Scan(&id, &firstname, &lastname)
 		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+	}
+	return
+}
+
+type userInput struct {
+	option wmenu.Opt
+}
+
+func (u *userInput) optFunc(option wmenu.Opt) error {
+	u.option = option
+	return nil
+}
+
+func createMenu(p string, m []string, u *userInput) {
+	menu := wmenu.NewMenu(p)
+	menu.ChangeReaderWriter(os.Stdin, os.Stdout, os.Stderr)
+	for i, m := range m {
+		menu.Option(m, i, false, u.optFunc)
+
+	}
+	err := menu.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	prompt := "Please select an option"
+	menuitems := []string{"View entries in database", "Add entry to database"}
+	u := &userInput{}
+	createMenu(prompt, menuitems, u)
+	switch u.option.ID {
+	case 0:
+		view()
+	case 1:
+		add()
 	}
 }
